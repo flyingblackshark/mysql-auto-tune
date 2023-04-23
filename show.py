88bc4c3bc0133a70987b5f8f4cda9b07107df482
FlyingBlackShark <aliu2000@outlook.com>
2023-04-23 14:07:16 +0800

    show in modules

--- show.py
+++ show.py
@@ -43,54 +43,58 @@ def baseline_test():
 
 if __name__ == '__main__':
     st.title('MySQL Auto Tuning System')
-    if os.path.isfile("res_all.csv"):
-        data = load_data(200)
-        history_data = data#.loc[::-1].reset_index(drop=True)
-        st.subheader("历史调优数据")
-        metric_chart_options = ["latency","read","write"]
-        metric_chart_select = st.multiselect("选择查看的指标",metric_chart_options)
+    history_tab, tune_tab, test_tab,settings_tab = st.tabs(["历史数据模块", "调优模块", "测试模块","设置模块"])
+    with history_tab:
+        if os.path.isfile("res_all.csv"):
+            data = load_data(200)
+            history_data = data#.loc[::-1].reset_index(drop=True)
+            st.subheader("历史调优数据")
+            metric_chart_options = ["latency","read","write"]
+            metric_chart_select = st.multiselect("选择查看的指标",metric_chart_options)
 
-        chart_data = pd.DataFrame(
-            history_data[['latency','read','write']],
-            columns=metric_chart_select)
-        st.line_chart(chart_data)
+            chart_data = pd.DataFrame(
+                history_data[['latency','read','write']],
+                columns=metric_chart_select)
+            st.line_chart(chart_data)
 
-        best_knobs = pd.DataFrame(data.iloc[-1,1:-1])
-        st.subheader("上次找到的最优knobs及其指标数值")
-        chart_data = st.dataframe(
-            best_knobs,
+            best_knobs = pd.DataFrame(data.iloc[-1,1:-1])
+            st.subheader("上次找到的最优knobs及其指标数值")
+            chart_data = st.dataframe(
+                best_knobs,
+                )
+        else:
+            st.subheader("暂无历史数据")
+    with tune_tab:
+        st.subheader("选择调优指标")
+        metric_options = ["Average_Latency", "Read", "Write"]
+        selected_metric = st.selectbox("选择指标", metric_options)
+        st.subheader("选择负载类型")
+        load_options = ["Read_Write", "Read_Only", "Write_Only"]
+        selected_load = st.selectbox("选择负载", load_options)
+        if st.button("开始调优"):
+            st.write("启动调优")
+            tune_pipeline()
+        rfmodel.res_output = st.empty()
+    with test_tab:
+        if st.button("进行基准性能测试"):
+            base_line = baseline_test()
+            st.dataframe(base_line,)
+    with settings_tab:
+        if st.button("初始化测试数据库"):
+            load_workload()
+        if st.button("清除优化参数并重启数据库"):
+            mydb = mysql.connector.connect(
+            host=mysql_ip,
+            user=mysql_user,
+            password=mysql_password
             )
-    else:
-        st.subheader("暂无历史数据")
-
-    st.subheader("选择调优指标")
-    metric_options = ["Average_Latency", "Read", "Write"]
-    selected_metric = st.selectbox("选择指标", metric_options)
-    st.subheader("选择负载类型")
-    load_options = ["Read_Write", "Read_Only", "Write_Only"]
-    selected_load = st.selectbox("选择负载", load_options)
+            knob_cursor = mydb.cursor()
+            knob_cursor.execute("RESET PERSIST;")
+            print("RESET PERSIST")
+            restart_db()
 
 
-    if st.button("初始化测试数据库"):
-        load_workload()
-    if st.button("清除优化参数并重启数据库"):
-        mydb = mysql.connector.connect(
-        host=mysql_ip,
-        user=mysql_user,
-        password=mysql_password
-        )
-        knob_cursor = mydb.cursor()
-        knob_cursor.execute("RESET PERSIST;")
-        print("RESET PERSIST")
-        restart_db()
-    if st.button("进行基准性能测试"):
-        base_line = baseline_test()
-        st.dataframe(base_line,)
-
-    if st.button("开始调优"):
-        st.write("启动调优")
-        tune_pipeline()
-    rfmodel.res_output = st.empty()
+  
 
 
     
